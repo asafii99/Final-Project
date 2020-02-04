c71c57da6f13be3b8a03ea436a8f7a657e48bee7
Abdullah Safi <abdullah.safi44@gmail.com>
2020-02-04 12:12:11 -0500

    made changes to the project.tf file

--- project.tf
+++ project.tf
@@ -142,6 +142,13 @@ resource "aws_security_group" "employee" {
   }
 
   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
